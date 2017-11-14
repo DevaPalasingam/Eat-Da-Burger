09ea000b5f0f20ccf39ddfd634b715c4c2aec832
Deva <deva.palasingam@gmail.com>
2017-11-13 23:05:53 -0600

    lots of debugging to do

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name, devoured) VALUES ("Jalapeno Burger", false);
+INSERT INTO burgers (name, devoured) VALUES ("Chicken Sandwich", false);
+INSERT INTO burgers (name, devoured) VALUES ("Mystery Meat", false);
+INSERT INTO burgers (name, devoured) VALUES ("Veggie Burger", false);
+INSERT INTO burgers (name, devoured) VALUES ("Hot Dog", false);
+INSERT INTO burgers (name, devoured) VALUES ("Jonah Burger", false);
